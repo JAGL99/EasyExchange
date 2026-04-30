81f50e0a2df82ca22a19b49591b4ad2756641e51
Julián Guzmán <jguzmanlongoria2@gmail.com>
2026-04-30 13:01:31 -0600

    Add correct colors and font to project

--- app/src/main/java/com/jagl/exchangeapp/ui/theme/Type.kt
+++ app/src/main/java/com/jagl/exchangeapp/ui/theme/Type.kt
@@ -2,33 +2,35 @@ package com.jagl.exchangeapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.jagl.exchangeapp.R
 
-// Set of Material typography styles to start with
+
+val interFontFamily = FontFamily(Font(R.font.inter_regular, FontWeight.Normal))
+val manropeFontFamily = FontFamily(Font(R.font.manrope_regular, FontWeight.Normal))
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    headlineLarge = TextStyle(
+        fontFamily = manropeFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = interFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = interFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
