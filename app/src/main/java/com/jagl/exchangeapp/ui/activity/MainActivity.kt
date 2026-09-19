c83586d6ea701af5da77b949c0a034ea66b9e78f
Julián Guzmán <jguzmanlongoria2@gmail.com>
2026-09-19 16:20:19 -0600

    Remove login screen

--- app/src/main/java/com/jagl/exchangeapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/jagl/exchangeapp/ui/activity/MainActivity.kt
@@ -27,7 +27,8 @@ class MainActivity() : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val internetConnectionState = viewModel.internetConnection.collectAsState(initial = NetworkStatus.Idle)
+            val internetConnectionState =
+                viewModel.internetConnection.collectAsState(initial = NetworkStatus.Idle)
             LaunchedEffect(internetConnectionState.value) {
                 if (internetConnectionState.value == NetworkStatus.Unavailable) {
                     Toast.makeText(
@@ -46,9 +47,7 @@ class MainActivity() : ComponentActivity() {
                 Surface(
                     modifier = Modifier.Companion.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    AppNavigation(viewModel.getToken())
-                }
+                ) { AppNavigation() }
             }
         }
     }
