9ff1825d8ec34c8f21e9c9782212b96344498e54
Julián Guzmán <jguzmanlongoria2@gmail.com>
2026-09-23 16:46:12 -0600

    Update app version from 0.1.3 to 0.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 android {
     val versionMajor = 0
     val versionMinor = 1
-    val versionPatch = 3
+    val versionPatch = 4
     val currencCompileSdk = 36
     val currentMinSdk = 24
 
